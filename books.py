e5c4f37612f7119515ce3cadeb87740d97bd448e
JAFARY <jaffarymdegela@gmail.com>
2025-09-15 16:16:17 +0300

    path-parametes

--- books.py
+++ books.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 BOOOKS = [
-    {'title':'Title First', 'author':'Authour 1', 'categories':'science'},
+    {'title':'Title One', 'author':'Authour 1', 'categories':'science'},
     {'title':'Title Two', 'author':'Authour 2', 'categories':'arts'},
     {'title':'Title Three', 'author':'Authour 3', 'categories':'physics'},
     {'title':'Title Four', 'author':'Authour 4', 'categories':'agriculture'},
@@ -12,6 +12,16 @@ BOOOKS = [
 ]
 
 @app.get("/books")
-
 async def first_app():
     return BOOOKS
+
+
+@app.get('/books/mybooks')
+async def read_all_books():
+    return {'book-title':'My favorate books'}
+
+@app.get("/books/{book_title}")
+async def read_book(book_title:str):
+    for book in BOOOKS:
+        if book.get('title').casefold()== book_title.casefold():
+            return book
