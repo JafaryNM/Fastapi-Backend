030e6c9c1b1a67637e1edabad2fb64a63556e7ca
JAFARY <jaffarymdegela@gmail.com>
2025-09-18 19:14:00 +0300

    Path parameter Validation

--- books2.py
+++ books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 from pydantic import BaseModel,Field,ConfigDict
 from typing  import Optional,List
 
@@ -58,7 +58,7 @@ async def read_books():
     return BOOKS
 
 @app.get("/books/{book_id}")
-async def read_book(book_id:int):
+async def read_book(book_id:int=Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -101,7 +101,7 @@ async def update_book(book:BookRequest):
 
 
 @app.delete("/books/{book_id}")
-async def delete_book(book_id:int):
+async def delete_book(book_id:int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
