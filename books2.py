739da856e38ae23ee3d9e855aee5a395180eb357
JAFARY <jaffarymdegela@gmail.com>
2025-09-18 12:24:55 +0300

    setup-books

---
+++ books2.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Book:
+    id:str
+    title:str
+    author:str
+    description:str
+    rating:int
+
+    def __init__(self, id , title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+
+BOOKS = [
+    Book(1,"Computer Science", "Jafary Mdegela", "This is good book for computer programmng", 5),
+    Book(2,"Next frontend boo", "John Samson", "This is book for frontend-framework", 4),
+    Book(3,"React js", "Book for reactjs", "This is good book for frontendbook", 5),
+    Book(4,"Django Backend", "Stephen Grider", "Good for backend full stack", 3),
+    Book(5,"Flask Api Developement", "John Doe", "This is for backend developement", 3),
+    Book(6,"Backend Nodejs", "Juma Tone", "This is Flask Testing", 3)
+]
+
+@app.get("/books")
+async def read_books():
+    return BOOKS
