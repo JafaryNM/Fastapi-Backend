6cdd31bdef1f1e3140ab7a665b4d90be0a3593ac
JAFARY <jaffarymdegela@gmail.com>
2025-09-20 16:02:02 +0300

    post-todos

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -5,6 +5,7 @@ from TodoApp.models import Todo
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
+from pydantic import BaseModel,Field
 
 app = FastAPI()
 
@@ -20,6 +21,15 @@ def get_db():
 
 db_dependency =Annotated[Session,Depends(get_db)]
 
+class TodoRequest(BaseModel):
+    title:str = Field(min_length=3)
+    description:str = Field(min_length=3 , max_length=100)
+    priority:int = Field(gt=0 , lt=6)
+    Complete:bool
+
+
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db:db_dependency):
     return db.query(Todo).all()
@@ -31,3 +41,11 @@ async def read_todo(db:db_dependency, todo_id:int=Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404,detail="Todo is not found")
+
+
+@app.post("/todos", status_code=status.HTTP_201_CREATED)
+async def create_todo(db:db_dependency,todo_request:TodoRequest):
+    todo_model =Todo(**todo_request.model_dump())
+    
+    db.add(todo_model)
+    db.commit()
